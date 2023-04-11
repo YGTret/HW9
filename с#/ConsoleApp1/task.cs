729189b5b9765a2eb2ee3841c437382144d2db6c
Yaroslav <yas.tretel@gmail.com>
2023-04-11 06:34:38 +0300

    Задача выполнена

---
+++ с#/ConsoleApp1/task.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+string[] arrIn = new string[0]; // Массив входных строк
+string[] arrOut = new string[0]; // Массив выходных строк
+string s = ""; // вводимая строка
+int sizeIn = 0; // Размер входного массива
+int sizeOut = 0; // Размер выходного массива
+
+Console.WriteLine("Введите строки (пустая строка завершает ввод):");
+s = Console.ReadLine();
+while (s != "")
+{
+    sizeIn++;
+    Array.Resize<string>(ref arrIn, sizeIn);
+    arrIn[sizeIn - 1] = s;
+    s = Console.ReadLine();
+}
+
+foreach (var item in arrIn)
+{
+    if (item.Length <= 3)
+    {
+        sizeOut++;
+        Array.Resize<string>(ref arrOut, sizeOut);
+        arrOut[sizeOut - 1] = item;
+    }
+}
+
+Console.WriteLine("Результат:");
+foreach(var item in arrOut)
+{
+    Console.WriteLine("{0}", item);
+}
